da830229cbe504cf5d2432ad7ce404d65329e03b
mcbarinov <mcbarinov@proton.me>
2024-12-25 15:21:37 +0300

    Remove cli #12

--- src/mm_solana/cli/cmd/example_cmd.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import click
-
-
-def run(module: str) -> None:
-    example_file = Path(Path(__file__).parent.absolute(), "../examples", f"{module}.yml")
-    click.echo(example_file.read_text())
